a61f918b74b8cfe6b3a00f6e15d627e93ababdfc
Tan Erizawa <studiomalaka@gmail.com>
2025-10-10 01:32:01 +0700

    Remove unused CashAccount imports from project controllers

--- app/Http/Controllers/ProjectExpenseController.php
+++ app/Http/Controllers/ProjectExpenseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\ProjectExpense;
-use App\Models\CashAccount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
